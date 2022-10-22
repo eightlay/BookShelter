538bc464b3c32fc199b7e8b62ac552f8be2b61db
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2022-10-22 19:51:15 +0400

    docstrings added

--- src/Book.py
+++ src/Book.py
@@ -4,6 +4,10 @@ from src.settings import _REQUIRED_FIELDS
 
 
 class Book:
+    """Class to store book's info. 
+    
+    General and required book fields are listed in `data/entities.json`
+    """
     def __init__(self, params: dict[str, str]) -> None:
         if all(f in params for f in _REQUIRED_FIELDS):
             self.from_dict(params)
@@ -11,18 +15,38 @@ class Book:
             raise Exception("Add required fields to create book object")
 
     def match(self, pattern: dict[str, str]) -> bool:
+        """Check if book matches patter
+
+        Args:
+            pattern (dict[str, str]): pattern to match with
+
+        Returns:
+            bool: does book match patter or not
+        """
         return all(v == self.__dict__.get(k) for k, v in pattern.items())
 
     def update(self, params: dict[str, str]) -> None:
+        """Update book fields with new data
+
+        Args:
+            params (dict[str, str]): new data
+        """
         self.from_dict(params)
 
     def to_dict(self) -> dict[str, str]:
+        """Book to dict
+
+        Returns:
+            dict[str, str]: book's fields
+        """
         return self.__dict__.copy()
 
     def from_dict(self, params: dict[str, str]) -> None:
         self.__dict__.update(params)
 
     def print(self) -> None:
+        """Print book's fields
+        """
         pprint(self.__dict__)
 
     def __str__(self) -> str:
--- src/BookShelter.py
+++ src/BookShelter.py
@@ -7,10 +7,14 @@ from src.Book import Book
 
 
 class BookShelter:
+    """Record keeping class for personal library
+    """
     def __init__(self) -> None:
         self._load_books()
         
     def _load_books(self) -> None:
+        """Load books from `data/books.csv` file
+        """
         self._books: dict[int, Book] = {}
         
         with open(os.path.join(_BOOKS_PATH), "r+") as file:
@@ -23,13 +27,31 @@ class BookShelter:
 
     @property
     def books(self) -> dict[int, Book]:
+        """Get tracked books
+
+        Returns:
+            dict[int, Book]: tracked books
+        """
         return self._books.copy()
     
     def next_id(self) -> int:
+        """Generate next book id and return it
+
+        Returns:
+            int: new book id
+        """
         self.cid += 1
         return self.cid
 
     def add(self, params: dict[str]) -> bool:
+        """Add book to library
+
+        Args:
+            params (dict[str]): new book's params
+
+        Returns:
+            bool: new book successfully added
+        """
         try:
             book = Book(params)
             self._books[self.next_id()] = book
@@ -38,12 +60,34 @@ class BookShelter:
         return True
 
     def remove(self, bid: int) -> None:
+        """Remove book from the library by its id
+
+        Args:
+            bid (int): book id
+        """
         del self._books[bid]
 
     def get(self, bid: int) -> Book:
+        """Get book by its id
+
+        Args:
+            bid (int): book id
+
+        Returns:
+            Book
+        """
         return self._books.get(bid)
 
     def update(self, bid: int, params: dict[str, str]) -> bool:
+        """Update tracked book info
+
+        Args:
+            bid (int): book id
+            params (dict[str, str]): new info
+
+        Returns:
+            bool: successfully updated
+        """
         if bid not in self._books:
             return False
         
@@ -51,6 +95,8 @@ class BookShelter:
         return True
 
     def save_close(self) -> None:
+        """Write data to `data/books.csv`
+        """
         with open(os.path.join(_BOOKS_PATH), "w+") as file:
             for bid, book in self._books.items():
                 file.write(f"{bid},\"{book.to_dict()}\"\n")
