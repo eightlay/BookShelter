a9d0d2b6db6a6e30e228de66e096fc9abd55f531
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2022-10-22 20:08:32 +0400

    docstrings added

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,14 @@
 import json
+from typing import Annotated
 
 _BOOKS_PATH = "./data/books.csv"
+"""Path to books storage"""
 _ENTITIES_PATH = "./data/entities.json"
+"""Path to entities file"""
 _LANGUAGE_NAME = "RUS"
+"""Code of the language to use"""
 
-
+"""Create variables from entities"""
 with open(_ENTITIES_PATH, "r") as file:
     for var, data in json.load(file).items():
         exec(f"{var} = {data}")
